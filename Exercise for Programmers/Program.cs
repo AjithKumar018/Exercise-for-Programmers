35702d80121019a835d40c85a8b09575c927c50a
Antonyajith <antonyajithkumar18@gmail.com>
2021-12-08 12:56:04 +0530

    Added: New Class

--- Exercise for Programmers/Program.cs
+++ Exercise for Programmers/Program.cs
@@ -10,7 +10,7 @@ namespace Exercise_for_Programmers
     {
         static void Main(string[] args)
         {
-            Exercise24 sol = new Exercise24();
+            Exercise23 sol = new Exercise23();
             sol.Solution();
         }
     }
